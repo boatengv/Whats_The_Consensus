3a98756a2cc1547b51ba591768291893f2d24546
Victor Boateng <boatengvictor3@gmail.com>
2026-03-07 11:29:25 +0000

    Reverting changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import SimpleLineIcons from "@react-native-vector-icons/simple-line-icons";
 import { Tabs } from "expo-router";
 import React from "react";
@@ -11,18 +12,43 @@ export default function TabLayout() {
         tabBarStyle: {
           backgroundColor: "white",
         },
+=======
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+>>>>>>> parent of b447f13 (Improving the home page, notification page)
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
+<<<<<<< HEAD
           tabBarIcon: ({}) => (
             <SimpleLineIcons name="home" size={25} color="black" />
+=======
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+>>>>>>> parent of b447f13 (Improving the home page, notification page)
           ),
         }}
       />
       <Tabs.Screen
+<<<<<<< HEAD
         name="alerts"
         options={{
           title: "Alerts",
@@ -81,6 +107,14 @@ export default function TabLayout() {
             fontWeight: "bold",
             fontSize: 24,
           },
+=======
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+>>>>>>> parent of b447f13 (Improving the home page, notification page)
         }}
       />
     </Tabs>
